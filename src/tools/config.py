3c83d2d37662c3a2c516026f6d658aba09d713f9
Kristóf Káldy <kaldykristof@gmail.com>
2023-12-12 15:41:12 +0100

    add support for json as config file format

--- src/tools/config.py
+++ src/tools/config.py
@@ -1,6 +1,12 @@
+from pathlib import Path
+import json
 from configparser import ConfigParser
 
 def parse(config_file_path):
-    config = ConfigParser()
-    config.read(config_file_path)
+    if Path(config_file_path).suffix == '.json':
+        with open(config_file_path, 'r') as file:
+            config = json.load(file)
+    else:
+        config = ConfigParser()
+        config.read(config_file_path)
     return config['mysql']
